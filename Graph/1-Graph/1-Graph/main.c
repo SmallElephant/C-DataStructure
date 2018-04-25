e388deab9984d554a72ef58034f560ddc3c8e952
xiangdefei <xiangdefei@we.com>
2018-04-25 15:31:01 +0800

    图的广度优先遍历实现

--- Graph/1-Graph/1-Graph/main.c
+++ Graph/1-Graph/1-Graph/main.c
@@ -9,7 +9,9 @@
 #include <stdio.h>
 #include "Graph.h"
 
-int visit[5];
+#define MaxSize 5
+
+int visit[MaxSize];
 
 void Visit(int v) {
     printf("%d->",v);
@@ -24,6 +26,31 @@ void DFS(AGraph *graph,int v) {
             DFS(graph, p->adjvex);
         }
         p = p->nextarc;
+    }
+}
+
+void BFS(AGraph *graph,int v,int visit[MaxSize]) {
+    int front = 0;
+    int rear = 0;
+    int que[MaxSize];
+    que[rear] = v; // 先进入队列，在赋值，也可以先更新rear，再赋值
+    rear = (rear + 1) % MaxSize;
+    Visit(v);
+    visit[v] = 1;
+    ArcNode *p;
+    while (front != rear) {
+        int j = que[front]; // 顶点出队列，保持与队列的操作一致，先取值，后更新位置
+        front = (front + 1) % MaxSize; // 出队，pushBack
+        p = graph->adjlist[j].firstarc;
+        while (p != NULL) {
+            if (visit[p->adjvex] == 0) { // 节点没有被访问，将所有的孩子都添加进入队列中
+                visit[p->adjvex] = 1; // 标记为已访问
+                Visit(p->adjvex);
+                que[rear] = p->adjvex; // 入队 push
+                rear = (rear + 1) % MaxSize;
+            }
+            p = p->nextarc;
+        }
     }
 }
 
@@ -43,22 +70,42 @@ int main(int argc, const char * argv[]) {
     // A B  C D
     // C D
     // D E
+
+//    VNode aNode = {'0',NULL};
+//    VNode bNode = {'1',NULL};
+//    VNode cNode = {'2',NULL};
+//    VNode dNode = {'3',NULL};
+//    VNode eNode = {'4',NULL};
+//    ArcNode arc1 = {1,NULL,0};
+//    ArcNode arc2 = {2,NULL,0};
+//    ArcNode arc3 = {3,NULL,0};
+//    ArcNode arc4 = {4,NULL,0};
+//    aNode.firstarc = &arc1;
+//    cNode.firstarc = &arc3;
+//    dNode.firstarc = &arc4;
+//    insertNextArcNode(&aNode, &arc2);
+//    insertNextArcNode(&aNode, &arc3);
+//    AGraph graph = {5,5,{aNode,bNode,cNode,dNode,eNode}};
+//    DFS(&graph, 0);
+//    printf("\n");
+    // A B D
+    // B C E
     VNode aNode = {'0',NULL};
     VNode bNode = {'1',NULL};
     VNode cNode = {'2',NULL};
     VNode dNode = {'3',NULL};
     VNode eNode = {'4',NULL};
-    ArcNode arc1 = {1,NULL,0};
-    ArcNode arc2 = {2,NULL,0};
-    ArcNode arc3 = {3,NULL,0};
-    ArcNode arc4 = {4,NULL,0};
+    ArcNode arc1 = {1,NULL,0}; // B
+    ArcNode arc2 = {2,NULL,0}; // C
+    ArcNode arc3 = {3,NULL,0}; // D
+    ArcNode arc4 = {4,NULL,0}; // E
     aNode.firstarc = &arc1;
-    cNode.firstarc = &arc3;
-    dNode.firstarc = &arc4;
-    insertNextArcNode(&aNode, &arc2);
     insertNextArcNode(&aNode, &arc3);
+    bNode.firstarc = &arc2;
+    insertNextArcNode(&aNode, &arc4);
     AGraph graph = {5,5,{aNode,bNode,cNode,dNode,eNode}};
-    DFS(&graph, 0);
+    int bfs[MaxSize] = {0};
+    BFS(&graph, 0, bfs);
     printf("\n");
     return 0;
 }
