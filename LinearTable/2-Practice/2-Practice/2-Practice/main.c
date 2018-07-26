e22bf9f424a2273f4994132c442a3fcddf1bd97d
xiangdefei <xiangdefei@we.com>
2018-07-26 10:30:39 +0800

    linear table

---
+++ LinearTable/2-Practice/2-Practice/2-Practice/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  2-Practice
+//
+//  Created by FlyElephant on 2018/7/26.
+//  Copyright © 2018年 FlyElephant. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
